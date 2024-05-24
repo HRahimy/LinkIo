472725772f07623bb4698dd9246b597b208a44e2
HRahimy <rahimy.hamza@gmail.com>
2024-05-24 17:12:17 +0300

    Add link created event handler

---
+++ src/Application/Links/EventHandlers/LinkCreatedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using LinkIo.Domain.Events;
+using Microsoft.Extensions.Logging;
+
+namespace LinkIo.Application.Links.EventHandlers;
+public class LinkCreatedEventHandler : INotificationHandler<LinkCreatedEvent>
+{
+    private readonly ILogger<LinkCreatedEventHandler> _logger;
+    public LinkCreatedEventHandler(ILogger<LinkCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(LinkCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("LinkIo Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
