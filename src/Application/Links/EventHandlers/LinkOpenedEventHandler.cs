40ef67795ffb0297b0987b295abf13f8bead0ef7
HRahimy <rahimy.hamza@gmail.com>
2024-05-25 16:08:09 +0300

    Add event handler for opening links

---
+++ src/Application/Links/EventHandlers/LinkOpenedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using LinkIo.Domain.Events;
+using Microsoft.Extensions.Logging;
+
+namespace LinkIo.Application.Links.EventHandlers;
+public class LinkOpenedEventHandler : INotificationHandler<LinkOpenedEvent>
+{
+    private readonly ILogger<LinkOpenedEventHandler> _logger;
+    public LinkOpenedEventHandler(ILogger<LinkOpenedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(LinkOpenedEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
