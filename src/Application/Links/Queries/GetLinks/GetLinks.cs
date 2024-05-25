8f1f5b8a4bcfc17e5924f1df8d6037c21078acea
HRahimy <rahimy.hamza@gmail.com>
2024-05-25 17:05:40 +0300

    Get query to list links

---
+++ src/Application/Links/Queries/GetLinks/GetLinks.cs
@@ -0,0 +1,33 @@
+﻿using LinkIo.Application.Common.Interfaces;
+using LinkIo.Application.Common.Mappings;
+using LinkIo.Application.Common.Models;
+using LinkIo.Application.Common.Security;
+using LinkIo.Application.Links.Models;
+using LinkIo.Domain.Constants;
+
+namespace LinkIo.Application.Links.Queries.GetLinks;
+[Authorize(PermissionScope = Permissions.ReadLinks)]
+public record GetLinksQuery : IRequest<PaginatedList<LinkDto>>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetLinksQueryHandler : IRequestHandler<GetLinksQuery, PaginatedList<LinkDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetLinksQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<LinkDto>> Handle(GetLinksQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Links
+            .ProjectTo<LinkDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
---
+++ src/Application/Links/Queries/GetLinks/GetLinksQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkIo.Application.Links.Queries.GetLinks;
+public class GetLinksQueryValidator : AbstractValidator<GetLinksQuery>
+{
+    public GetLinksQueryValidator()
+    {
+        RuleFor(v => v.PageNumber)
+            .GreaterThanOrEqualTo(1).WithMessage("PageNumber at least greater than or equal to 1.");
+
+        RuleFor(v => v.PageSize)
+            .GreaterThanOrEqualTo(10).WithMessage("PageSize at least greater than or equal to 10.");
+    }
+}
