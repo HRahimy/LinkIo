41e661e67b9605ed1dff27c00e3b2f9f3462f7a3
HRahimy <rahimy.hamza@gmail.com>
2024-05-26 12:21:26 +0300

    Fix auth interceptor logic

--- src/Web/ClientApp/src/app/shared/interceptors/auth.interceptor.ts
+++ src/Web/ClientApp/src/app/shared/interceptors/auth.interceptor.ts
@@ -8,7 +8,9 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      authService.loginWithRedirect();
+      if (error.status === 401) {
+        authService.loginWithRedirect();
+      }
       return throwError(() => error);
     })
   );
